55b9b323dcf051ae6ff02e3c9da32e1e7f10d881
odadev <timo@jeske.email>
2018-07-09 23:09:54 +0200

    Stand CubeCON #7 (bzw. Eugene Webseite erstellen Reihe #37)

--- menu.php
+++ menu.php
@@ -1,13 +1,13 @@
 <?php if (!defined('IN_SITE')) { echo "Zugriff verweigert!"; die(); } ?>
 
 <ul>
-    <li><a href="index.php?section=startseite">Startseite</a></li>
-    <li><a href="index.php?section=videos">Videos</a></li>
-    <li><a href="index.php?section=ueber_mich">Über mich</a></li>
-    <li><a href="index.php?section=impressum">Impressum</a></li>
-    <li><a href="index.php?section=login">Login</a></li>
+    <li><a href="startseite">Startseite</a></li>
+    <li><a href="videos">Videos</a></li>
+    <li><a href="ueber_mich">Über mich</a></li>
+    <li><a href="impressum">Impressum</a></li>
+    <li><a href="login">Login</a></li>
     <?php if($db->isUserLoggedIn()) { ?>
-        <li><a href="index.php?section=write_news">News schreiben</a></li>
-        <li><a href="index.php?section=show_news">News</a></li>
+        <li><a href="write_news">News schreiben</a></li>
+        <li><a href="show_news">News</a></li>
     <?php } ?>
 </ul>
